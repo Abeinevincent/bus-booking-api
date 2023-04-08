6f7e278d103f322699b835a1d6c17363b0c3746a
Abeinevincent <abeinevincent@gmail.com>
2023-04-08 12:55:49 +0300

    More Updates On FPN

--- routes/fpn.js
+++ routes/fpn.js
@@ -1,13 +1,15 @@
-const { default: axios } = require("axios");
-
 const router = require("express").Router();
 
 router.post("/", async (req, res) => {
   try {
-    const response = await axios.post(`${process.env.FPN_URL}`, {});
-    console.log(response.data);
+    console.log(req);
+    console.log("FPN came successfully")
+    const { test } = req.body;
+    return res.status(200).json(test);
+    // ttddd
   } catch (err) {
     console.log(err);
+    return res.status(500).json(err);
   }
 });
 
